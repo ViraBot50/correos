c94739aa765b97c13993a4c96255bce17a2f6821
ViraBot50 <josebrayansaldanaaguado@gmail.com>
2025-10-08 22:20:40 -0600

    ingreso de documentos de entrada y salida

--- src/main/java/org/example/Document.java
+++ src/main/java/org/example/Document.java
@@ -1,10 +1,35 @@
 package org.example;
 
+import java.io.*;
+
 public class Document {
 
 
     public String m_getText(String p_ruta){
-        return "hello word";
+        String v_respuesta="",v_linea;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(p_ruta));
+            while ((v_linea = br.readLine()) != null) {
+                v_respuesta+=v_linea+"\n";
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+
+        v_respuesta=v_respuesta.trim();
+
+        return v_respuesta;
     }
+
+    public void m_writeText(String p_ruta, String p_msg){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(p_ruta))) {
+            bw.write(p_msg);
+            bw.write("Otra línea");
+            System.out.println("Archivo escrito correctamente");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Document v_document=new Document();
-        System.out.println(v_document.m_getText("input/correos.bin"));
-
+        String v_txtOptenido=v_document.m_getText("src/main/resources/input/correos.txt");
+        v_document.m_writeText("src/main/resources/output/correos.txt",v_txtOptenido);
 
     }
 }
