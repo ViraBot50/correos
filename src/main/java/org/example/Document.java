e672445b0ae8da20b2b2e27eb529f021624ca12e
ViraBot50 <josebrayansaldanaaguado@gmail.com>
2025-10-08 21:49:40 -0600

    Primer commit del proyecto correos

---
+++ src/main/java/org/example/Document.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Document {
+
+
+    public String m_getText(String p_ruta){
+        return "hello word";
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Document v_document=new Document();
+        System.out.println(v_document.m_getText("input/correos.bin"));
+
+
+    }
+}
